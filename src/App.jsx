a07e7914710daab0d4fbf3a34e0d513e6f87e6db
JiriResler <j.resler.29@gmail.com>
2023-09-24 09:02:47 +0200

    add log in logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,54 @@
+import { useState } from 'react';
+import {
+  useSession,
+  LoginButton,
+  LogoutButton,
+} from "@inrupt/solid-ui-react";
+
 function App() {
+  const { session } = useSession();
+
+  const identityProviders = [
+    'https://solidcommunity.net/',
+    'https://login.inrupt.com/',
+    'https://inrupt.net/',
+    'https://solidweb.org/'
+  ];
+
+  const [selectedProvider, setSelectedProvider] = useState(identityProviders[0]);
+
   return (
     <>
-      Hello world
+      <h2>GH Pages deployment test</h2>
+      <div>
+        isLoggedIn: {session.info.isLoggedIn == true ? "true" : "false"}
+      </div>
+      <br />
+      <div>
+        Your webID: {session.info.isLoggedIn == true ? session.info.webId : "Log in to see your WebID"}
+      </div>
+      <br />
+      <div>
+        <select
+          value={selectedProvider}
+          onChange={e => setSelectedProvider(e.target.value)}
+        >
+          {identityProviders.map((providerUrl) => {
+            return (
+              <option key={providerUrl}>
+                {providerUrl}
+              </option>
+            );
+          })}
+        </select>
+        {' '}
+        <LoginButton
+          oidcIssuer={selectedProvider}
+          redirectUrl={window.location.href}
+        />
+        {' '}
+        <LogoutButton />
+      </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { SessionProvider } from "@inrupt/solid-ui-react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SessionProvider sessionId="some-id">
+      <App />
+    </SessionProvider>
   </React.StrictMode>,
 )
